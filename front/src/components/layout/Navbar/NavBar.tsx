d97276d447d7cbe9da698d32ff37dcff96c761d9
Nicolas <nicorojas1@hotmail.com>
2025-08-08 18:58:30 -0500

    Arquitectura

--- front/src/components/layout/Navbar/NavBar.tsx
+++ front/src/components/layout/Navbar/NavBar.tsx
@@ -1,11 +1,11 @@
-import { NavMenu } from "./NavigationMenur";
+import { NavMenu } from "./NavigationMenu";
 
  
 const Navbar= () => {
   return ( 
   <div className="flex  flex-row justify-between p-3 items-center bg-gray-300">
     <div>
-      <h1>Home Hero</h1>
+      <h1 className="font-Name font-bold logo text-3xl ">Home Hero</h1>
     </div>
     <NavMenu/>
 
