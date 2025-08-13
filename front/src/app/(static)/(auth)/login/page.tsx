8680e0f1f6579fae99531b54659d73ea3c816942
agustinNistal <agustin.nistal88@gmail.com>
2025-08-13 19:47:50 -0300

    signinA

--- front/src/app/(static)/(auth)/login/page.tsx
+++ front/src/app/(static)/(auth)/login/page.tsx
@@ -2,33 +2,37 @@ import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
 import Image from "next/image"
+import NameLogo from "@/components/Logo/logo"
+import { Card, CardContent } from "@/components/ui/card"
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
+          <a href="/" className="flex items-center gap-2 font-medium">
+          <NameLogo/>
           </a>
         </div>
-        <div className="flex flex-1 items-center justify-center">
+        <div className="flex items-center justify-center flex-1">
           <div className="w-full max-w-xs">
+            <Card>
+              <CardContent>
+
             <LoginForm />
+              </CardContent>
+
+            </Card>
           </div>
         </div>
       </div>
       <div className="relative hidden bg-muted lg:block">
         <Image
-          src=""
+          src="https://ik.imagekit.io/ankxi835d/Home%20Hero/limpieza.jpg"
           alt="Image"
-          width={1920}
-          height={1080}
           fill
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="object-cover"
+          sizes="100vw, 100vh"
         />
       </div>
     </div>
