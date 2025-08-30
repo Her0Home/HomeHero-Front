894964b9dca2ff9477dc28ca402c9cad775b6299
Nicolas <nicorojas1@hotmail.com>
2025-08-30 15:55:45 -0500

    pefil

--- front/src/app/(dashboard)/dashboard/(profile)/components/PerfilCard.tsx
+++ front/src/app/(dashboard)/dashboard/(profile)/components/PerfilCard.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardHeader,
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { IUser } from "@/types/users";
 import Image from "next/image";
 import { FunctionComponent } from "react";
@@ -17,27 +20,45 @@ const PerfilCard: FunctionComponent<PerfilCardProps> = ({ user }) => {
     <div className="w-2/4">
       <Card className="flex flex-col ">
         <CardHeader className="items-center">
-          <div>
-            <div className="bg-slate-300 w-60 h-60 rounded-full flex justify-center items-center overflow-hidden relative">
-              {user?.imageProfile && user.imageProfile.length > 300 ? (
-                <Image
-                  alt={user?.name ?? " "}
-                  src={user.imageProfile}
-                  width={58}
-                  height={58}
-                  className="object-cover rounded-full"
-                />
-              ) : (
-                <p className="text-hero-gray font-Title text-9xl text-center break-words px-2">
-                  {user?.name
-                    ?.split(" ")
-                    .map((word) => word[0])
-                    .join("")
-                    .toUpperCase()}
-                </p>
-              )}
-            </div>
-          </div>
+          <Dialog>
+            <DialogTrigger>
+              <div>
+                <div className="bg-slate-300 w-60 h-60 rounded-full flex justify-center items-center overflow-hidden relative">
+                  {user?.imageProfile && user.imageProfile.length > 300 ? (
+                    <Image
+                      alt={user?.name ?? " "}
+                      src={user.imageProfile}
+                      width={58}
+                      height={58}
+                      className="object-cover rounded-full"
+                    />
+                  ) : (
+                    <p className="text-hero-gray font-Title text-9xl text-center break-words px-2">
+                      {user?.name
+                        ?.split(" ")
+                        .map((word) => word[0])
+                        .join("")
+                        .toUpperCase()}
+                    </p>
+                  )}
+                </div>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+
+              </DialogHeader>
+              <DialogDescription>
+                <Input type="file"/>
+                
+              </DialogDescription>
+              <DialogFooter>
+                <Button>
+                  Guardar
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </CardHeader>
         <CardContent className="flex flex-col text-center font-Text gap-2">
           <p className="text-4xl logo font-Title">{user?.name}</p>
@@ -45,12 +66,14 @@ const PerfilCard: FunctionComponent<PerfilCardProps> = ({ user }) => {
         </CardContent>
         <CardFooter className="-mt-5">
           <Card className="max-w-sm mx-auto bg-white shadow-lg rounded-lg p-4 border border-gray-200">
-              <h2 className="text-lg font-semibold font-Text text-gray-800">
-                Cuenta:{" "}
-                {user?.isActive 
-                  ? <span className="text-green-400">Activa</span>
-                  : <span className="text-red-500">Temporalmente Desactivada</span>}
-              </h2>
+            <h2 className="text-lg font-semibold font-Text text-gray-800">
+              Cuenta:{" "}
+              {user?.isActive ? (
+                <span className="text-green-400">Activa</span>
+              ) : (
+                <span className="text-red-500">Temporalmente Desactivada</span>
+              )}
+            </h2>
           </Card>
         </CardFooter>
       </Card>
